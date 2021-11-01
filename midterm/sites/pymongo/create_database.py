f8d1e2d574c3b84103bb60203c333d89ee168528
miyunecadz <jvcadz@gmail.com>
2021-11-01 10:11:16 +0800

    Update midterm client

--- midterm/sites/pymongo/create_database.py
+++
@@ -1,8 +0,0 @@
-# import pymongo package
-import pymongo
-
-# instantiate the mongodb
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-
-# database connection
-mydb = client["mydatabase"]
